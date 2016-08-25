4bf811e0918265256f3844f2075ea63f49cad08e
Putz <1175624968@mil>
2016-08-25 15:23:10 -0600

    Updated data with accumulator and live histogram

--- fluidized_particle.py
+++ fluidized_particle.py
@@ -10,6 +10,12 @@ from tkinter import ttk
 from tkinter.colorchooser import askcolor
 from PIL import Image, ImageTk
 
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+
 class LogType(Enum):
     EVENT = 1
     SET = 2
@@ -111,6 +117,39 @@ def make_hsv_circlefinder(hsv_low, hsv_high, resized_size=None):
 
     return finder
 
+def empty_acc():
+    return dict(t=[], x=[], y=[], vx=[], vy=[])
+
+def accumulate_steps(acc, current):
+    """
+    Accumulate steps in the tracking of the ball.  While "current" is the most
+    recent output of the circlefinder function (which contains time hack, image, contours,
+    position, and intermediate steps), "acc" only tracks t, x, y, vx, and vy.
+    t, x, and y are taken from the "current" entry, but vx and vy are calculated
+    using the last entries in acc.
+    """
+#    print("Curr: ", current)
+    # if we're the first one, do a dummy
+    acc['t'].append(current['t'])
+    pos = current['position']
+    acc['x'].append(pos[0] if pos is not None else np.nan)
+    acc['y'].append(pos[1] if pos is not None else np.nan)
+
+    if len(acc['t']) == 1:
+        acc['vx'].append(np.nan)
+        acc['vy'].append(np.nan)
+    else:
+        dt = acc['t'][-2] - acc['t'][-1]
+        dx = acc['x'][-2] - acc['x'][-1]
+        dy = acc['y'][-2] - acc['y'][-1]
+
+        acc['vx'].append(dx/dt)
+        acc['vy'].append(dy/dt)
+
+    return acc
+
+    
+
 class HSVWidget(tk.Frame):
 
     def __init__(self, master, text, init_val=(0,0,0)):
@@ -181,7 +220,7 @@ class FrameTab(tk.Frame):
         
 class ColorChooserFrame(FrameTab):
 
-    def __init__(self, master, text):
+    def __init__(self, master, text, low, high):
 
         FrameTab.__init__(self, master, text)
         self.pack()
@@ -204,8 +243,10 @@ right-hand frame shows only the colored ping-pong ball""",
         self.high_hsv = HSVWidget(self.bars_frame, "High value", (91,255,255))
         self.high_hsv.pack(side=tk.LEFT)
         self.high_subscription = self.high_hsv.val_stream.subscribe(self.set_high)
-        self.set_high({'h':0, 's':0, 'v':0})
-        self.set_low({'h':0, 's':0, 'v':0})
+
+        self.stream = Subject()
+        self.low_match = low
+        self.high_match = high
 
         self.thumb_frame = tk.Frame(self)
         self.thumb_frame.pack()
@@ -229,14 +270,19 @@ right-hand frame shows only the colored ping-pong ball""",
         #self.throttled_camera.dispose()
         print("disposing color mask subscriptions")
         self.cam_subscription.dispose()
-    
+
+    def broadcast_range(self):
+        self.stream.on_next(dict(low=self.low_match, high=self.high_match))
+        
     def set_low(self, d):
         """d should be a dict keyed by hsv"""
         self.low_match = (d['h'],d['s'],d['v'])
+        self.broadcast_range()
 
     def set_high(self, d):
         """d should be a dict keyed by hsv"""
         self.high_match = (d['h'],d['s'],d['v'])
+        self.broadcast_range()
 
     def on_new_image(self, d):
         # probably only update this if focused
@@ -249,14 +295,13 @@ right-hand frame shows only the colored ping-pong ball""",
             mask = cv2.inRange(hsv, self.low_match, self.high_match)
             mask = cv2.erode(mask, None, iterations=2)
             mask = cv2.dilate(mask, None, iterations=2)
-            print (self.low_match, self.high_match)
             self.mask_image = ImageTk.PhotoImage(Image.fromarray(mask))
             self.live_thumb.config(image = self.thumb_image)
             self.mask_thumb.config(image = self.mask_image)
 
 class LiveVideoFrame(FrameTab):
 
-    def __init__(self, master, text):
+    def __init__(self, master, text, low, high):
 
         FrameTab.__init__(self, master, text)
         self.pack()
@@ -265,8 +310,6 @@ class LiveVideoFrame(FrameTab):
                                        command = self.acquire_background)
         self.update_button.pack()
 
-        self.lower_match = (35,59,54)
-        self.upper_match = (91,255,255)
 
         blackarr = np.zeros((THUMBHEIGHT,THUMBWIDTH))
         self.thumb = ImageTk.PhotoImage(Image.fromarray(blackarr))
@@ -284,6 +327,8 @@ class LiveVideoFrame(FrameTab):
         self.latest_thumb = None
 
         self.feed_sub = None
+
+        self.circlefinder = make_hsv_circlefinder(low, high)
 
         self.bind("<Destroy>", self.dispose_subscriptions)
 
@@ -309,6 +354,9 @@ class LiveVideoFrame(FrameTab):
                 self.latest_image = im
                 self.set_monitor()
 
+    def on_new_limits(self, limits):
+        self.circlefinder = make_hsv_circlefinder(limits['low'], limits['high'])
+                
     def acquire_background(self):
         self.background = None
         self.background_thumb = None
@@ -317,9 +365,8 @@ class LiveVideoFrame(FrameTab):
         
     def set_monitor(self):
         # im should be a numpy array
-        finder = make_hsv_circlefinder(self.lower_match, self.upper_match)
 
-        this_step = finder(self.last_step)
+        this_step = self.circlefinder(self.last_step)
 
         thumb = cv2.resize(this_step["steps"][0], THUMB_SIZE)
         self.latest_thumb = thumb
@@ -332,10 +379,102 @@ class LiveVideoFrame(FrameTab):
         self.diff_thumb = ImageTk.PhotoImage(dim)
         self.outline.configure(image = self.diff_thumb)
 
+class DataAcquisitionFrame(FrameTab):
 
+    def __init__(self, master, text, low, high):
+        FrameTab.__init__(self, master, text)
+
+        self.start_button = tk.Button(self, text="Start Acquisition",
+                                      command=self.start_acquisition)
+        self.start_button.grid(row=0,column=0)
+        self.stop_button = tk.Button(self, text="Stop Acquisition",
+                                     command=self.stop_acquisition)
+        self.stop_button.grid(row=0,column=1)
+
+        self.thumb_width = 320
+        self.thumb_height = 240
+
+        blackarr = np.zeros((self.thumb_height, self.thumb_width))
+        self.monitor_image = ImageTk.PhotoImage(Image.fromarray(blackarr))
+        self.monitor = tk.Label(self, image=self.monitor_image)
+        self.monitor.grid(row=1,column=0)
+
+        self.stats_frame = tk.Frame(self)
+        self.num_points = tk.Label(self.stats_frame, text="Num Points:")
+        self.num_points.pack()
+        self.stats_frame.grid(row=1,column=1)
+
+        self.v_fig = Figure(figsize=(3,2), dpi=100)
+        self.v_hist = FigureCanvasTkAgg(self.v_fig, master=self)
+        self.v_hist.show()
+        self.v_hist.get_tk_widget().grid(row=2,column=0)
+
+        self.x_fig = Figure(figsize=(3,2), dpi=100)
+        self.x_hist = FigureCanvasTkAgg(self.x_fig, master=self)
+        self.x_hist.show()
+        self.x_hist.get_tk_widget().grid(row=2,column=1)
+
+        self.data_subscription = None
+        self.camera_stream = None
+        self.step_stream = None
+        self.step_subscription = None
+
+        self.circlefinder = make_hsv_circlefinder(low,high)
+
+    def on_new_limits(self, limits):
+        self.circlefinder = make_hsv_circlefinder(limits['low'], limits['high'])
+    
+    def start_acquisition(self):
+        self.dispose_subscriptions()
+
+        self.step_stream = self.camera_stream.map(self.circlefinder)
+        self.step_subscription = self.step_stream.subscribe(self.on_new_step)
+        self.data_subscription = self.step_stream.scan(accumulate_steps, seed=empty_acc()).subscribe(self.on_new_data)
+
+    def on_new_step(self, step):
+        if self.is_selected() :
+            thumb = cv2.resize(step['steps'][0], (self.thumb_width, self.thumb_height))
+            self.image = ImageTk.PhotoImage(Image.fromarray(thumb))
+            self.monitor.config(image=self.image)
+
+    def update_v_hist(self, acc):
+        a = self.v_fig.gca()
+        a.clear()
+        a = self.v_fig.add_subplot(111)
+        v_components = np.array([x for x in zip(acc['vx'], acc['vy']) if (not np.isnan(x[0])) and (not np.isnan(x[1]))])
+        vs = np.sqrt((v_components*v_components).sum(1))
+        n, bins, patches = a.hist(vs, normed=1)
+        a.set_title("Normalized hist of |v|")
+        self.v_fig.tight_layout()
+        self.v_hist.draw()
+
+    def update_x_hist(self, acc):
+        a = self.x_fig.gca()
+        
+    def on_new_data(self, acc):
+        self.num_points.config(text="Num Points: {0}".format(len(acc['t'])))
+        EVERY=10
+        if len(acc['t']) % EVERY == 0:
+            self.update_v_hist(acc)
+
+    def dispose_subscriptions(self):
+        if self.step_stream is not None:
+            self.step_stream = None
+        if self.step_subscription is not None:
+            self.step_subscription.dispose()
+            self.step_subscription = None
+        if self.data_subscription is not None:
+            self.data_subscription.dispose()
+            self.data_subscription = None
+        
+    def stop_acquisition(self):
+        self.dispose_subscriptions()
+        # ask to save data
+        
+        
 class TrackerGUI(object):
 
-    def __init__(self, master, camera):
+    def __init__(self, master, camera, low, high):
 
         self.notebook = ttk.Notebook(master)
         self.scheduler = rx.concurrency.TkinterScheduler(master)
@@ -343,14 +482,15 @@ class TrackerGUI(object):
         self.camera = camera
         self.feed_sub = None
 
-        self.color_chooser_frame = ColorChooserFrame(self.notebook, "Color Chooser")
-        
-
-        self.live_video_frame = LiveVideoFrame(self.notebook, "Live Video")
+        self.color_chooser_frame = ColorChooserFrame(self.notebook, "Color Chooser", low, high)
+        self.live_video_frame = LiveVideoFrame(self.notebook, "Live Video", low, high)
+        self.data_acquisition_frame = DataAcquisitionFrame(self.notebook, "Data Acquisition", low, high)
 
         self.wire_subscriptions(camera)
         self.notebook.add(self.color_chooser_frame, text=self.color_chooser_frame.text)
         self.notebook.add(self.live_video_frame, text=self.live_video_frame.text)
+        self.notebook.add(self.data_acquisition_frame, text = self.data_acquisition_frame.text)
+        
         self.notebook.pack()
         self.notebook.bind("<Destroy>", self.on_destroy)
         self.start_feed()
@@ -358,6 +498,9 @@ class TrackerGUI(object):
     def wire_subscriptions(self, camera):
         self.live_video_frame.subscribe_to_camera_stream(camera.image_stream, 200, self.scheduler)
         self.color_chooser_frame.subscribe_to_camera_stream(camera.image_stream, 500, self.scheduler)
+        self.color_chooser_frame.stream.subscribe(self.live_video_frame.on_new_limits)
+
+        self.data_acquisition_frame.camera_stream = camera.image_stream
 
     def on_destroy(self, event):
         print("stopping feed")
@@ -389,7 +532,9 @@ if __name__ == "__main__":
     cam.start_capture()
 
     root = tk.Tk()
-    app = TrackerGUI(root, cam)
+    low = (35,59,54)
+    high = (91,255,255)
+    app = TrackerGUI(root, cam, low, high)
     root.mainloop()
 
     cam.stop_capture()
